da7f989b18a79de61b835a46fef61c7816edade7
ripper <ripperhe@qq.com>
2017-01-06 18:43:45 +0800

    tu

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ZYCircleProgressView
 圆形进度条 🎾
+
+![](https://raw.githubusercontent.com/ripperhe/Resource/master/20170106/circleprogressview.png)
--- ZYCircleProgressViewDemo/ViewController.m
+++ ZYCircleProgressViewDemo/ViewController.m
@@ -21,23 +21,32 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
+    // 示例 1
     ZYCircleProgressView *progressView = [[ZYCircleProgressView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
+    progressView.progress = 0.3;
     [self.view addSubview:progressView];
     self.progressView = progressView;
+
+
+    // 示例 2
+    ZYCircleProgressView *progressView2 = [[ZYCircleProgressView alloc] initWithFrame:CGRectMake(100, 250, 100, 100)];
+    [self.view addSubview:progressView2];
     
-    self.progressView.progress = 0.3;
+    progressView2.progress = 0.7;
+    [progressView2 updateConfig:^(ZYCircleProgressViewConfig *config) {
+        config.backLineColor = [UIColor lightGrayColor];
+        config.progressLineColor = [UIColor orangeColor];
+        config.lineWidth = 2;
+        config.startAngle = - (M_PI + M_PI_4);
+        config.endAngle = M_PI_4;
+    }];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
 {
     [self.progressView updateConfig:^(ZYCircleProgressViewConfig *config) {
-        config.backLineColor = [UIColor lightGrayColor];
-        config.progressLineColor = [UIColor orangeColor];
         config.lineWidth = 3;
-        config.lineCap = kCALineCapRound;
-        config.startAngle = - (M_PI + M_PI_4);
-        config.endAngle = M_PI_4;
+        config.lineCap = kCALineCapSquare;
     }];
 
     self.progressView.progress = 0.8;
